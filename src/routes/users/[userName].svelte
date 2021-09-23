4f691db9c2c36cb9f75ab5c998850f8e320b2066
miyatsuki <miyatsuki.shiku@gmail.com>
2021-09-23 17:41:26 +0900

    とりあえずサンプルのogp情報入れてみる

--- src/routes/users/[userName].svelte
+++ src/routes/users/[userName].svelte
@@ -20,6 +20,23 @@
   export let questLog;
 </script>
 
+<svelte:head>
+  <meta name="twitter:card" content="summary" />
+  <meta
+    property="og:url"
+    content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/"
+  />
+  <meta property="og:title" content="A Twitter for My Sister" />
+  <meta
+    property="og:description"
+    content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling."
+  />
+  <meta
+    property="og:image"
+    content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
+  />
+</svelte:head>
+
 <span>{userName} の 勉強ログ</span>
 
 {#each questLog as quest (quest.id)}
